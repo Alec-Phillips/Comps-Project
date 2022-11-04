587184d1cb361ca583e86cebbbb6b6242e7010a5
Alec-Phillips <70179843+Alec-Phillips@users.noreply.github.com>
2022-11-03 23:08:57 -0700

    re-style option buttons
    use linear gradient to improve style and
    add check mark to completed problems

--- project-code/src/ContentOptionButton.js
+++ project-code/src/ContentOptionButton.js
@@ -1,16 +1,34 @@
 import styled from 'styled-components';
 
 import lockImg from './lock-image.png';
+import checkImg from './check-mark-image.png';
 
 const StyledOption = styled.div`
-  background: transparent;
-  background-color: ${props => props.completed ? 'lightgreen' : props.active ? 'rgb(192, 228, 240)' : 'rgb(192, 228, 240)'};
+  // background: transparent;
+  background: linear-gradient(
+    to bottom,
+    white,
+    rgba(192, 228, 240, 1)
+  );
+  // background-color: ${props => props.completed ? 'lightgreen' : props.active ? 'rgb(192, 228, 240)' : 'rgb(192, 228, 240)'};
   ${
     props => props.locked ? `
     background-image: url(${lockImg});
     opacity: .3;
     ` : null
   }
+  ${
+    props => props.completed ? `
+    background-image: linear-gradient(
+      to bottom,
+      rgba(192, 228, 240, 0.1),
+      rgba(192, 228, 240, 1)
+    ), url(${checkImg});
+    // opacity: 1;
+    // background-color: rgb(192, 228, 240);
+    ` : null
+  }
+  
   // background-image: url(${lockImg});
   transform: ${props => props.active ? 'scale(1.2)' : 'scale(1)'};
   border-radius: 5px;
--- project-code/src/OptionButton.js
+++ project-code/src/OptionButton.js
@@ -2,7 +2,12 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 const StyledOption = styled.div`
-    background: transparent;
+    // background: transparent;
+    background: linear-gradient(
+      to bottom,
+      white,
+      rgba(192, 228, 240, 1)
+    );
     background-color: ${props => props.active ? 'rgb(192, 228, 240)' : 'rgb(192, 228, 240)'};
     transform: ${props => props.active ? 'scale(1.1)' : 'scale(1)'};
     border-radius: 5px;
